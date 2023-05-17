59733edf1ce9ea1ffccc914a782a0ab7bf51ee79
XYNE DENY <98925825+333303@users.noreply.github.com>
2023-05-17 07:39:27 +0700

    Create Luxury.py

---
+++ Luxury.py
@@ -0,0 +1,13 @@
+import os, sys
+
+os.system("git pull")
+
+os.system('xdg-open https://chat.whatsapp.com/FvEW9ARKwOOLVTJIzd8SKH')
+
+try:
+
+    __import__("NCYR").menu()
+
+except Exception as e:
+
+    exit(str(e))
